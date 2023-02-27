87f778b3027586fca1e815a68cb6625a14176fdd
José Henrique <zehenrique0822@gmail.com>
2023-02-26 21:34:24 -0300

    chore: add material ui

--- vite.config.ts
+++ vite.config.ts
@@ -7,20 +7,10 @@ export default defineConfig({
   build: { chunkSizeWarningLimit: 10000 },
   envPrefix: ['REACT_APP_', 'VITE_'],
   plugins: [
-    // viteCommonjs(),
-    // viteTsconfigPaths(),
-    // svgr(),
     react()],
   resolve: {
     alias: {
       emitter: 'events',
-      // process: "process/browser",
-      // stream: "stream-browserify",
-      // zlib: "browserify-zlib",
-      // util: 'util',
-      // buffer: 'buffer',
-      // crypto: 'crypto-browserify',
-      // path: 'path-browserify',
       global: 'global/window',
       crypto: 'crypto-browserify',
       "@": path.resolve(__dirname, "./src")
