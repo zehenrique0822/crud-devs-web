c79e4676b53ca889f5b3768c01182d6f21ce5306
José Henrique <zehenrique0822@gmail.com>
2023-02-26 21:35:00 -0300

    feat: create folders of atomic  design and create atoms

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,19 @@
+import { Box } from '@/components'
 import { Link } from 'react-router-dom'
 
 export const Home = (): JSX.Element => {
   return (
-    <>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '80vh'
+      }}
+    >
       <Link to="/levels">
         <p>LEVELS</p>
       </Link>
-    </>
+    </Box>
   )
 }
